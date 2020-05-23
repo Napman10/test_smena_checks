1a53e8880bacf5974d6d36f02df03c9644e475d6
me <ilya.kashapov99@gmail.com>
2020-05-23 19:21:43 +0500

    created models

--- TestApp/models.py
+++ TestApp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
+
+CHECK_TYPES = (
+    ('client', 'Client'),
+    ('kitchen', 'Kitchen')
+)
+
+ORDER_STATUSES = (
+    ('new', 'New'),
+    ('rendered', 'Rendered'),
+    ('printed', 'Printed')
+)
+
+class Printer(models.Model):
+    name = models.CharField(null=False, max_length=100)
+    api_key = models.CharField(null=False, max_length=100)
+    check_type = models.CharField(max_length=10, choices=CHECK_TYPES)
+    point_id = models.IntegerField(null=False)
+    
+class Check(models.Model):
+    printer_id = models.ForeignKey(Printer)
+    type = models.CharField(max_length=10, choices=CHECK_TYPES)
+    order = models.JSONField()
+    status = models.CharField(max_length=10, choices=ORDER_STATUSES)
+    pdf_file = models.FileField(upload_to='pdf/')
+    
 
 # Create your models here.
