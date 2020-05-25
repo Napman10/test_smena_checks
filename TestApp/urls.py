868e8926034fc5bef0cc6474e36f2a37099dfb20
me <ilya.kashapov99@gmail.com>
2020-05-25 19:37:16 +0500

    добавил urls

--- TestApp/urls.py
+++ TestApp/urls.py
@@ -3,4 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^create_checks/$', views.create_checks, name='create_checks'),
+    url(r'^rendered_checks/$', views.rendered_checks, name='rendered_checks'),
+    url(r'^take_pdf/$', views.take_pdf, name='take_pdf'),
 ]
